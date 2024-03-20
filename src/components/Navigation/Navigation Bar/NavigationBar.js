06c74ca23891e932e63662dc1e724cf296464e80
adiyat-shihab <mohammadshihab104@gmail.com>
2024-03-21 03:26:28 +0600

    create footer

--- src/components/Navigation/Navigation Bar/NavigationBar.js
+++ src/components/Navigation/Navigation Bar/NavigationBar.js
@@ -7,6 +7,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -15,15 +16,16 @@ import NavigationTabs from "@/components/Navigation/Navigation Tabs/NavigationTa
 import { cn } from "@/lib/utils";
 import NavigationTeam from "@/components/Navigation/Navigation Team/NavigationTeam";
 import NavigationMenu from "@/components/Navigation/Navigation Menu/NavigationMenu";
+import { Button } from "@/components/ui/button";
 
 /**
  * The NavigationBar component displays the navigation tabs and the company logo.
  */
 const NavigationBar = () => {
   return (
-    <Card>
+    <Card className={"w-[382px] flex flex-col  "}>
       <CardHeader className={"px-4 pt-2"}>
-        <div className={"flex gap-16 items-center"}>
+        <div className={"flex gap-16 items-center justify-between"}>
           <div className={"flex gap-2 items-center"}>
             <Image
               src={"/main-logo.png"}
@@ -53,6 +55,41 @@ const NavigationBar = () => {
         <NavigationTeam />
         <NavigationMenu />
       </CardContent>
+      <CardFooter className={" mt-auto flex flex-col space-y-1.5  "}>
+        <div
+          className={
+            "-mx-1.5 hover:bg-[#F4F5F6] w-full rounded-[8px] transition duration-150 cursor-pointer"
+          }
+        >
+          <NavigationTabs
+            title={"Invite Member"}
+            icon={"/Navigation Icons/user-add.svg"}
+          />
+        </div>
+        <div
+          className={
+            "-mx-4 hover:bg-[#F4F5F6] w-full rounded-[8px] transition duration-150 cursor-pointer"
+          }
+        >
+          <NavigationTabs
+            title={"Help and first steps"}
+            icon={"/Navigation Icons/question-mark.svg"}
+          />
+        </div>
+        <div
+          className={
+            "flex items-center  justify-between px-2  py-2 bg-[#F4F5F6]  hover:bg-[#F4F5F6] w-full rounded-[12px] transition duration-150 "
+          }
+        >
+          <div className={"flex  gap-2 items-center"}>
+            <div className={"px-2 py-0.5 bg-white rounded-lg"}>
+              <p className={"font-semibold"}>7</p>
+            </div>
+            <h1 className={"font-semibold"}>days left on trail</h1>
+          </div>
+          <Button className={"rounded-[10px]"}>Add Billing</Button>
+        </div>
+      </CardFooter>
     </Card>
   );
 };
--- src/components/Navigation/Navigation Menu/NavigationMenu.js
+++ src/components/Navigation/Navigation Menu/NavigationMenu.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import DropdownContainer from "@/components/Navigation/Navigation Dropdown/DropdownContainer";
+import NavigationTabs from "@/components/Navigation/Navigation Tabs/NavigationTabs";
 
 const NavigationMenu = () => {
   const list1 = [
@@ -11,6 +12,7 @@ const NavigationMenu = () => {
     "Add new sub",
   ];
   const list2 = ["Sales", "Products", "Add new sub"];
+  const list3 = ["Design", "Products", "Add new sub"];
   return (
     <div>
       <div className={"flex justify-between pl-2 cursor-default  py-4"}>
@@ -28,6 +30,27 @@ const NavigationMenu = () => {
       <div className={"space-y-1.5"}>
         <DropdownContainer title={"Products"} list={list1} />
         <DropdownContainer title={"Sales"} list={list2} />
+        <DropdownContainer title={"Design"} list={list3} />
+        <div
+          className={
+            "-mx-1.5 hover:bg-[#F4F5F6] rounded-[8px] transition duration-150 cursor-pointer"
+          }
+        >
+          <NavigationTabs
+            title={"Office"}
+            icon={"/Navigation Icons/folder.svg"}
+          />
+        </div>{" "}
+        <div
+          className={
+            "-mx-1.5 hover:bg-[#F4F5F6] rounded-[8px] transition duration-150 cursor-pointer"
+          }
+        >
+          <NavigationTabs
+            title={"Legal"}
+            icon={"/Navigation Icons/folder.svg"}
+          />
+        </div>
       </div>
     </div>
   );
